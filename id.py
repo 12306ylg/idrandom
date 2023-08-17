6b3024e30257efdea5cf87a1a24a0164e7eab08e
电棍老板 <89052130+mixueicecreamandtea@users.noreply.github.com>
2023-08-17 11:06:29 +0800

    Update id.py

--- id.py
+++ id.py
@@ -1,10 +1,14 @@
+# -*- coding: utf-8 -*-
 import random
-location=input("location:")
-date=input("date:")
-setround=input("round:")
-save=open(idsave, mode='a')#以追加方式写入
-while(round < setround):
- housiwei=(random.randint(1000, 9999))
- huodedesfz=(str(location),str(date),str(housiwei))
- save.write(huodedesfz)
- round=(round+1)
+location=int(input("location:"))
+date=int(input("date:"))
+setround=int(input("round:"))
+round=(0)
+save=open("idsave", mode='a+', encoding="utf-8")#以追加方式写入
+while round < setround :
+       housiwei=(random.randint(1000, 9999))
+       huodedesfz=("身份证号"+str(location)+str(date)+str(housiwei)+"\n")
+       save.write(str(huodedesfz))
+       print(huodedesfz)
+       round=(round+1)
+print("done")
